d47a517a4226a4ee5aa869bbe8fb07f76543b75e
八基 後藤 <220385@jc-21.jp>
2023-08-25 10:50:51 +0900

    プロジェクト ファイルを追加します。

---
+++ Engine/SceneManager.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "GameObject.h"
+
+//ƒV[ƒ“‚ğ—…—ñ
+enum SCENE_ID {
+	SCENE_ID_TEST = 0,
+};
+class SceneManager : public GameObject
+{
+	SCENE_ID currentSceneID_, nextSceneID_;
+public:
+	SceneManager(GameObject* parent);
+	~SceneManager();
+	void Initialize() override;
+	void Update() override;
+	void Draw() override;
+	void Release() override;
+	void ChangeScene(SCENE_ID _next);
+};
+
---
+++ TestScene.cpp
@@ -0,0 +1,22 @@
+#include "TestScene.h"
+#include "Engine/SceneManager.h"
+#include "Engine/Input.h"
+TestScene::TestScene(GameObject* parent):GameObject(parent, "TestScene")
+{
+}
+
+void TestScene::Initialize()
+{
+}
+
+void TestScene::Update()
+{
+}
+
+void TestScene::Draw()
+{
+}
+
+void TestScene::Release()
+{
+}
