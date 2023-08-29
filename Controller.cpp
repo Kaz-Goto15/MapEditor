a51e8c34cd2b91476b62341f01972b8c2da9cd80
八基 後藤 <220385@jc-21.jp>
2023-08-29 11:51:47 +0900

    ctrl

--- Controller.cpp
+++ Controller.cpp
@@ -1,11 +1,11 @@
 #include "Controller.h"
 #include "Engine/Camera.h"
 #include "Engine/Input.h"
-
-
+#include "Engine/Debug.h"
 //コンストラクタ
 Controller::Controller(GameObject* parent) :
-    GameObject(parent, "Controller")
+    GameObject(parent, "Controller"),
+    movUnit_(0.1f)
 {
     CamPosition_ = { 0,3,-8 };
     CamTarget_ = { 0,2,0 };
@@ -27,12 +27,75 @@ void Controller::Initialize()
 void Controller::Update()
 {
     
-    if (Input::IsKey(DIK_A))CamTarget_.x--;
+    //戦車の現在位置をベクトル型に変換
+    XMVECTOR nowVec = XMLoadFloat3(&transform_.position_);
+    //1フレームの移動ベクトル
+    XMVECTOR movVec = { 0, 0, movUnit_, 0 };
+    XMVECTOR movVec2 = { movUnit_, 0, 0, 0 };
+    //Y軸でY回転量分回転させる行列
+    XMMATRIX rotMat = XMMatrixRotationY(XMConvertToRadians(transform_.rotate_.y));
+    //移動ベクトルを変形
+    movVec = XMVector3TransformCoord(movVec, rotMat);
 
+    //Rotate
+    if (Input::IsKey(DIK_LEFTARROW)) {
+        transform_.rotate_.y -= 1.0f;
+    }
+    if (Input::IsKey(DIK_RIGHTARROW)) {
+        transform_.rotate_.y += 1.0f;
+    }
+    //if (Input::IsKey(DIK_UPARROW)) {
+    //    XMFLOAT3  nrot = transform_.rotate_;
+    //    transform_.rotate_.x -= 1.0f;
+    //}
+    //if (Input::IsKey(DIK_DOWNARROW)) {
+    //    transform_.rotate_.x += 1.0f;
+    //}
 
+    //Press W to Move
+    if (Input::IsKey(DIK_W)) {
+        nowVec += movVec;
+        XMStoreFloat3(&transform_.position_, nowVec);
+        Debug::Log(transform_.position_.x);
+    }
+    //Press S to Back
+    if (Input::IsKey(DIK_S)) {
+        nowVec -= movVec;
+        XMStoreFloat3(&transform_.position_, nowVec);
+    }
+    //Press A to Left
+    if (Input::IsKey(DIK_A)) {
+        /*movVec2 *= XMVector3Normalize(XMLoadFloat3(&transform_.rotate_));*/
+        XMVECTOR b = XMVectorSet(0, 1, 0, 0);
+        XMVECTOR c = XMVector3Cross(movVec, b);    //cは(0, 1, 0)になる
 
-    Camera::SetPosition(CamPosition_);
-    Camera::SetTarget(CamTarget_);
+        //移動量追加
+        nowVec += c;
+        //現在位置をベクトルからtransform_.position_に戻す
+        XMStoreFloat3(&transform_.position_, nowVec);
+    }
+    //Press D to Back
+    if (Input::IsKey(DIK_D)) {
+        XMVECTOR b = XMVectorSet(0, 1, 0, 0);
+        XMVECTOR c = XMVector3Cross(movVec, b);    //cは(0, 1, 0)になる
+
+        //移動量追加
+        nowVec -= c;
+        //現在位置をベクトルからtransform_.position_に戻す
+        XMStoreFloat3(&transform_.position_, nowVec);
+    }
+
+    //if (Input::IsKey(DIK_A))CamTarget_.x--;
+
+    Camera::SetTarget(transform_.position_);
+    //カメラ位置->対象の後方
+    XMVECTOR vCam = { 0,5,-10,0 };                           //距離指定
+    vCam = XMVector3TransformCoord(vCam, rotMat);   //変形:回転
+    XMStoreFloat3(&CamPosition_, nowVec + vCam);          //XMFLOAT3変換:現在座標+回転変形後の距離
+    Camera::SetPosition(CamPosition_);                    //セット
+
+    //Camera::SetPosition(CamPosition_);
+    //Camera::SetTarget(CamTarget_);
 
 }
 
